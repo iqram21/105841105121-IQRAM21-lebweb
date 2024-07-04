b6c99f274ee290e8612bb1cb532bf50b06270305
iqram21 <105841105121@student.unismuh.ac.id>
2024-07-04 10:52:45 +0800

    Navigation dan taskbar

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+// In App.js in a new project
 
-const App = () => {
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignupPage from './SignupPage';
+import LoginPage from './LoginPage';
+import ForgetPasswordPage from './ForgetPasswordPage';
+
+
+function HomeScreen({ navigation }) {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Login" onPress={() => navigation.navigate('Login')}/>
+      <Button
+        title="Go to Forget Password" onPress={() => navigation.navigate('ForgetPassword')}/>
+      <Button
+        title="Go to Signup" onPress={() => navigation.navigate('Signup')}/>
     </View>
-  )
+  );
 }
 
-export default App
+const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({})
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Signup" component={SignupPage} />
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="ForgetPassword" component={ForgetPasswordPage} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
